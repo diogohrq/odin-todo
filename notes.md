15d900b4cd723e6aa0ade90a9ace7829763b4c22
diogohrq <diogohs110@gmail.com>
2024-01-13 21:48:55 -0300

    add webpack configuration

---
+++ notes.md
@@ -0,0 +1,20 @@
+## App Features
+
+- the user should be able to create, delete and edit todos.
+- todos should be grouped in projects.
+- when the app is first oppened there should be a default project, where all new todos goes.
+- the user should be able to create, delete and edit projects.
+
+### Todo Properties 
+
+- title
+- description
+- dueDate
+- priority
+- status
+
+### Project Properties
+
+- name 
+- todos
+
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: false
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html'
+    }),
+  ]
+}
