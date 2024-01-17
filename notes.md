c0ef46e0211d70d45e3c3b163f9d7835f4cb912c
diogohrq <diogohs110@gmail.com>
2024-01-17 08:04:47 -0300

    create basic style

--- notes.md
+++ notes.md
@@ -1,11 +1,13 @@
-## App Features
+# App
+
+## Features
 
 - the user should be able to create, delete and edit todos.
 - todos should be grouped in projects.
 - when the app is first oppened there should be a default project, where all new todos goes.
 - the user should be able to create, delete and edit projects.
 
-### Todo Properties 
+## Todo Properties 
 
 - id
 - title
@@ -14,9 +16,20 @@
 - priority
 - status
 
-### Project Properties
+## Project Properties
 
 - id
 - name 
 - todos
+
+## UI 
+
+### Colors
+
+- primary: #46C78C
+- secondary: #F2F2F2
+- accent: #64B5D0
+- neutral: #F6FCF9
+- text: #07180F
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    hot: false
-  },
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    }),
-  ]
-}
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+		hot: false,
+	},
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/html/template.html',
+		}),
+    new MiniCssExtractPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+		],
+	},
+};
